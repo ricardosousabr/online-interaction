99a192a7eff45152c2120c0018a5da8414b57dec
ricardo <ricardo.br.pi@gmail.com>
2022-04-26 21:26:27 -0300

    style: style the button component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/global.scss';
 import '../styles/home.scss';
 import '../styles/theme.scss';
+import '../styles/btntheme.scss';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
